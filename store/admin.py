35e2f2182e4729ef2166be5e3c80acb58a94b685
Aodan <hardy-a4@ulster.ac.uk>
2024-03-20 09:16:21 +0000

    adding images

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import Product, Order, OrderItem
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description', 'image_thumbnail')  # Add 'image_thumbnail' here
+    search_fields = ('name', 'description')
+    readonly_fields = ('image_thumbnail',)  # Add this line
+
+    def image_thumbnail(self, obj):
+        return obj.image.url if obj.image else ''
+
+    image_thumbnail.short_description = 'Thumbnail'  # Add this line
+
+
+
+admin.site.register(Product, ProductAdmin)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'date_ordered', 'status')
+    list_filter = ('status',)
+    search_fields = ('user__username',)
+    inlines = (OrderItemInline,)
+
+admin.site.register(Order, OrderAdmin)
--- store/models.py
+++ store/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_url = models.URLField()
+    image = models.ImageField(upload_to='product_images/', default='default_image.jpg')
 
     def __str__(self):
         return self.name
