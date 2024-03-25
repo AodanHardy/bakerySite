4b2467cf05b350a3369593c705d40346848ebfda
Aodan <hardy-a4@ulster.ac.uk>
2024-03-25 11:18:55 +0000

    base.html and home added

--- store/models.py
+++ store/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/', default='default_image.jpg')
+    image = models.ImageField(upload_to='static/product_images/', default='default_image.jpg')
 
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     STATUS_CHOICES = (
--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from store.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
